7d6f1bf85f47da7f483deabf8eb2af91245d2963
config-core913 <xin302452@gmail.com>
2026-06-12 16:09:39 +0800

    编辑员工员工业务代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -27,4 +27,7 @@ public interface EmployeeMapper {
     Page<Employee> pageQurey(EmployeePageQueryDTO employeePageQueryDTO);
 
     void update(Employee employee);
+
+    @Select("select * from employee where id = #{id}")
+    Employee getById(Long id);
 }
